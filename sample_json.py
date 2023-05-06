1d5766fecde435a69985791f3e18b20970f5b134
hebrew <daniel@sadogursky.com>
2023-05-06 18:32:45 -0400

    Add the training notebook

--- sample_json.py
+++ sample_json.py
@@ -3,11 +3,11 @@ import json
 import numpy as np
 import math
 
-with open("yelp_reviews_sample.json") as f:
+with open("yelp_academic_dataset_review.json") as f:
   records = [json.loads(x) for x in f]
 
 count = len(records)
-sample_ratio = 0.04
+sample_ratio = 0.25
 sample_count = math.ceil(count * sample_ratio)
 
 sample_indexes = np.random.choice(
